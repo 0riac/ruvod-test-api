a64d3bc6a97e8a1250589618e09f5adbfbacfd88
0riac <katan998@mail.ru>
2020-06-28 02:15:48 +0300

    fix: fix cors origin

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 module.exports = Object.assign({
   cors: {
-    origin: 'https://0riac.github.io/ruvod-test',
+    origin: 'https://0riac.github.io',
     credentials: true
   }
 }, require(`./env/${process.env.NODE_ENV}`));
