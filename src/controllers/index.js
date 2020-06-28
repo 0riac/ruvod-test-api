e92a4c2758bd3415de0be0f5e411bba1837929a3
0riac <katan998@mail.ru>
2020-06-28 14:41:54 +0300

    fix

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,7 +3,7 @@ const { Client } = require('schemes');
 const githubAuth = require('github-auth');
 // const config = require('../config');
 
-const githubMiddleware = githubAuth(process.env.GITHUB_ID, process.env.GITHUB_SECRET, {});
+const githubMiddleware = githubAuth(process.env.GITHUB_ID, process.env.GITHUB_SECRET, { users: ['0riac', 'mafintosh', 'octocat'] });
 
 const githubCallbackMiddleware = (req) => {
   console.log('github auth', req);
